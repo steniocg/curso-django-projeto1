99e7b6667d802367da5e7ab4fbd16b7f2843852d
Stênio Bezerra de Oliveira <stenioti.dev@gmail.com>
2024-06-20 09:45:09 -0300

    estilizando-html

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Stênio',
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'Stênio',
     })
